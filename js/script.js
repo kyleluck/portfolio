4839f6c4bb68f93204f5125b7f8ec1709a401c7c
Kyle Luck <luck.kyle@gmail.com>
2016-06-21 11:11:44 -0400

    initial files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.self-slice').hide();
+  $('.group1').fadeIn('slow');
+  setTimeout(function() {
+    $('.group2').fadeIn('slow');
+  }, 1000);
+  setTimeout(function() {
+    $('.group3').fadeIn('slow');
+  }, 2000);
+  setTimeout(function() {
+    $('.group4').fadeIn('slow');
+  }, 3000);
+});
