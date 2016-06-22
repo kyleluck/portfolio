865975497c3d72d033289c86fd7056942610d921
Kyle Luck <luck.kyle@gmail.com>
2016-06-22 09:07:35 -0400

    remove Home from nav; remove unnecessary JS

--- js/script.js
+++ js/script.js
@@ -1,40 +1,22 @@
 $(function() {
 
-  $('.self-slice').hide();
-  $('.group1').fadeIn('slow');
-  setTimeout(function() {
-    $('.group2').fadeIn('slow');
-  }, 1000);
-  setTimeout(function() {
-    $('.group3').fadeIn('slow');
-  }, 2000);
-  setTimeout(function() {
-    $('.group4').fadeIn('slow');
-  }, 3000);
-
-  $('#home').click(function() {
-    $('html,body').animate({
-      scrollTop: $(".jumbotron").offset().top},
-      'slow');
-  });
-
   $('#skills').click(function() {
-    $('html,body').animate({
-      scrollTop: $(".skills").offset().top},
-      'slow');
+    scrollToArea('.skills');
   });
 
   $('#projects').click(function() {
-    $('html,body').animate({
-      scrollTop: $(".projects").offset().top},
-      'slow');
+    scrollToArea('.projects');
   });
 
   $('#contact').click(function() {
-    $('html,body').animate({
-      scrollTop: $(".contact").offset().top},
-      'slow');
+    scrollToArea('.contact');
   });
+
+  function scrollToArea(areaName) {
+    $('html,body').animate({
+      scrollTop: $(areaName).offset().top},
+      'slow');
+  }
 
   //Check to see if the window is top if not then display button
 	$(window).scroll(function(){
