cce029b7aa641fb7bf58a33a4940af62849f3919
Kyle Luck <luck.kyle@gmail.com>
2016-06-24 14:36:23 -0400

    animate progress bars on scroll

--- js/script.js
+++ js/script.js
@@ -36,4 +36,33 @@ $(function() {
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
+
+  //animate progress bars
+  var stopAnimation = false;
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 400 && !stopAnimation) {
+      animateProgressBars(true);
+    } 
+  });
+
+  function animateProgressBars(doAnimation) {
+    if (doAnimation) {
+      stopAnimation = true;
+      $('.progress-bar').each(function() {
+        var thisBar = $(this);
+        var animatePercentage = thisBar.attr("data-percentage");
+        var currentPercentage = 0;
+
+        var progress = setInterval(function() {
+          if (currentPercentage >= animatePercentage) {
+            clearInterval(progress);
+          } else {
+            currentPercentage += 3;
+            thisBar.css('width', (currentPercentage) + '%');
+          }
+        }, 50);
+      });
+    }
+  }
 });
